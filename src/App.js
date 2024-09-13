1e83f43b3670cd560eef90d43c65a5aabce3515a
krystianjp <krystianjp97@gmail.com>
2024-09-13 09:01:15 +0100

    made servers sidebar design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import ServersBar from "./components/ServersBar";
+import "./index.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ServersBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ServersBar.js
@@ -0,0 +1,82 @@
+function ServersBar() {
+  return (
+    <div className="sidebar">
+      <div className="bonfire-text">
+        Bon<span>fire</span>
+      </div>
+      <div className="server-icon main-icon">
+        <img
+          src="https://i.postimg.cc/Ss8Nrrv4/bonfire-logo-transparent.png"
+          alt="bonfire logo"
+          className="server-icon-img main-icon-img"
+        />
+      </div>
+      <div className="hor-line"></div>
+      <div className="server-icon">
+        <img
+          src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
+          alt="server icon"
+          className="server-icon-img"
+        />
+      </div>
+      <div className="server-icon">
+        <img
+          src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
+          alt="server icon"
+          className="server-icon-img"
+        />
+      </div>
+      <div className="server-icon">
+        <img
+          src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
+          alt="server icon"
+          className="server-icon-img"
+        />
+      </div>
+      <div className="server-icon">
+        <img
+          src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
+          alt="server icon"
+          className="server-icon-img"
+        />
+      </div>
+      <div className="server-icon">
+        <img
+          src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
+          alt="server icon"
+          className="server-icon-img"
+        />
+      </div>
+      <div className="server-icon">
+        <img
+          src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
+          alt="server icon"
+          className="server-icon-img"
+        />
+      </div>
+      <div className="server-icon">
+        <img
+          src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
+          alt="server icon"
+          className="server-icon-img"
+        />
+      </div>
+      <div className="server-icon">
+        <img
+          src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
+          alt="server icon"
+          className="server-icon-img"
+        />
+      </div>
+      <div className="server-icon">
+        <img
+          src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
+          alt="server icon"
+          className="server-icon-img"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ServersBar;
