f7c59581da71fad682c647d2fafdd38d30cddd53
krystianjp <krystianjp97@gmail.com>
2024-09-15 14:27:40 +0100

    messages design

---
+++ src/components/Message.js
@@ -0,0 +1,60 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+function Message() {
+  return (
+    <div className="message-container-container">
+      <div className="message-container">
+        <div className="message-pfp">
+          <img
+            className="pfp-img"
+            src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
+            alt="user profile picture"
+          />
+        </div>
+        <span className="message-username">PickleJuice</span>
+        <span className="message-date">Today at 12:36</span>
+        <div className="message-content">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Arcu nostra
+          maecenas accumsan; cras nostra odio luctus. Faucibus consequat fusce
+          senectus, platea mattis pulvinar. Fermentum viverra natoque velit in
+          maecenas pellentesque volutpat consequat aliquam. At proin quam
+          potenti rutrum neque ultricies, arcu tempor. Pretium cubilia molestie
+          pellentesque tempor eu non. Platea eget ultricies laoreet platea
+          posuere. Imperdiet volutpat quisque justo nullam pulvinar convallis
+          gravida erat sodales. Habitasse ad curabitur curabitur massa integer,
+          nulla quisque hendrerit amet. Parturient aptent duis maximus inceptos
+          netus semper feugiat augue. Malesuada magnis erat aenean, hendrerit
+          eros donec nisi. Dis aliquam netus curae sodales scelerisque venenatis
+          etiam netus euismod. Euismod rhoncus facilisi, magnis habitasse montes
+          netus adipiscing in. Sed class aptent pulvinar, non lobortis ex
+          ligula. In urna natoque penatibus consequat etiam quam etiam.
+          Venenatis per varius duis dapibus condimentum. Rutrum consequat per
+          sapien himenaeos hendrerit. Sapien ultricies rhoncus dapibus, interdum
+          feugiat auctor ornare. Aliquet porttitor ullamcorper, vehicula a orci
+          proin auctor eu! Facilisis sapien fames efficitur neque erat orci
+          justo ornare consectetur. Mus risus ex commodo aliquam interdum
+          porttitor. Id ut dictum fusce ultricies cubilia convallis dictum
+          tortor. Dapibus sem sed scelerisque rhoncus sed. Facilisis sociosqu
+          quam fringilla; felis mus laoreet conubia dignissim. Praesent aliquam
+          taciti nisi nam ut pharetra hendrerit? Mattis pellentesque ligula
+          finibus erat rutrum leo vivamus varius primis. Nostra pretium
+          suspendisse facilisi massa nostra quis. Vehicula inceptos dictumst
+          viverra eget fermentum phasellus inceptos sit. Dictum luctus augue
+          bibendum sociosqu id. Convallis suscipit netus dignissim montes ante
+          purus. Lacus rhoncus dignissim senectus hac vulputate dis commodo.
+          Aptent rutrum eleifend porta montes blandit mus velit ante pharetra.
+          Eleifend vehicula ornare etiam nullam morbi porttitor pulvinar dis.
+          Condimentum feugiat pellentesque dui ex purus aenean. Sagittis ac eros
+          arcu consectetur dictum duis. Natoque nibh arcu viverra et nibh
+          tincidunt. Conubia lorem turpis porttitor posuere himenaeos. Nostra
+          hendrerit nunc lectus varius parturient mattis condimentum. Bibendum
+          himenaeos purus, sociosqu nascetur suspendisse dapibus aliquet. Fames
+          nostra blandit cursus purus, litora orci. Nunc velit penatibus ante
+          lectus potenti nisi. Litora dolor magnis nisl purus eu scelerisque
+          non. Litora ex sociosqu litora ipsum pellentesque ipsum.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Message;
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,5 +1,21 @@
+import Message from "./Message";
 function Messages() {
-  return <div className="messages-container"></div>;
+  return (
+    <div className="messages-bar">
+      <div className="messages-container">
+        <Message />
+        <Message />
+      </div>
+      <div className="message-input-container">
+        <span className="material-icons attach-file-icon">attach_file</span>
+        <input
+          type="text"
+          className="message-input"
+          placeholder="Message PickleJuice"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Messages;
