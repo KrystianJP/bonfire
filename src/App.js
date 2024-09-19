23cbef4b8db5cdbe53273286785ed11e5c81c3ea
krystianjp <krystianjp97@gmail.com>
2024-09-19 22:05:57 +0100

    settings page sidebar design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import ServersBar from "./components/ServersBar";
 import FriendsPage from "./components/FriendsPage";
 import ServerPage from "./components/ServerPage";
+import SettingsPage from "./components/Settings/SettingsPage";
 import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <ServersBar />
-      <ServerPage />
+      <SettingsPage />
     </div>
   );
 }
---
+++ src/components/Settings/SettingsPage.js
@@ -0,0 +1,21 @@
+function SettingsPage() {
+  return (
+    <div className="settings-page">
+      <div className="settings-side-bar-container">
+        <div className="settings-side-bar">
+          <div className="settings-group">Account</div>
+          <div className="settings-group">Privacy</div>
+          <div className="settings-group">Theme</div>
+          <div className="settings-group">Accessibility</div>
+          <div className="settings-group">Voice & Video</div>
+          <div className="settings-group">Log Out</div>
+        </div>
+      </div>
+      <div className="settings-page-content-container">
+        <div className="settings-page-content">yo</div>
+      </div>
+    </div>
+  );
+}
+
+export default SettingsPage;
