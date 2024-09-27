c167ad0b0e2f278ecb0543a38fbde329941c1686
krystianjp <krystianjp97@gmail.com>
2024-09-27 15:22:58 +0100

    fixed friend in friends list design

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -17,14 +17,16 @@ function FriendsList() {
         <div className="role-list">
           <div className="role-container">
             <Link to="/messages/SomeUsername" className="role">
-              <div className="friend-pfp">
-                <img
-                  className="pfp-img"
-                  src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
-                />
+              <div className="friend-left">
+                <div className="friend-pfp">
+                  <img
+                    className="pfp-img"
+                    src="https://i.pinimg.com/originals/d5/7c/eb/d57ceb9546385b8d5c224c34502ddcf6.jpg"
+                  />
+                </div>
+                <span className="friend-name">Friend 1</span>
               </div>
-              <span className="friend-name">Friend 1</span>
-              <span className="friend-icons unban-icon">
+              <span className="friend-icons">
                 <span className="material-icons ">message</span>
                 <span className="material-icons ">delete</span>
               </span>
